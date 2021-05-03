cfab0712166e1096f5b9e7b9e812c12007483808
Umar Waqas <m.umarwaqas@yahoo.com>
2021-05-03 16:03:54 +0500

    Os System Configuration

--- assets/js/widget-script.js
+++ assets/js/widget-script.js
@@ -37,6 +37,44 @@
 
       }
     });
-});
+  });
+
+  jQuery("#sc_user_url").submit(function(event) {
+    /* stop form from submitting normally */
+    event.preventDefault();
+    
+    /* get the action attribute from the form element */
+    var url = jQuery( this ).attr( 'action' );
+    
+    /* Send the data using post */
+    jQuery.ajax({
+      type: 'POST',
+      url: url,
+      data: {
+        action:   jQuery('#sc_user_url_action').val(),
+        login:    jQuery('#login').val(), 
+        signup:   jQuery('#signup').val(), 
+        nonce:    jQuery('#sc_nonce_field').val()
+      },
+      success: function (data, textStatus, XMLHttpRequest) {
+        //alert(data);
+
+        var timeleft = 3;
+        var downloadTimer = setInterval(function(){
+        timeleft--;
+        jQuery("#msg_u_u").text(timeleft);
+          if(timeleft <= 0){
+            jQuery("#msg_u_u").text(data).show();
+            clearInterval(downloadTimer);
+          }
+        },1000);
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+          //alert(errorThrown);
+          jQuery("#msg_u_u").text(data).show();
+
+      }
+    });
+  });
 
 })( jQuery );
--- widget.php
+++ widget.php
@@ -1,42 +1,38 @@
 <?php
 /**
- * Registration of the Admin dashboard widget
+ * Registration of the Admin dashboard website url widget
  */
 function sc_add_dashboard_widgets() {
-    wp_add_dashboard_widget(
-      'sc_system_configuration',          // Widget slug
-      __('System Configuration (Website URLs)', 'sc-system-configuration'), // Title
-      'sc_system_configuration'        // Display function
-    );  
+    // wp_add_dashboard_widget( 'sc_system_confige_website_url',  __('System Configuration (Website URLs)', 'sc-system-configuration'), 'sc_system_confige_website_url' );
+    add_meta_box( 'sc_system_confige_website_url', esc_html__( 'System Configuration (Website URLs)', 'sc-system-configuration' ), 'sc_system_confige_website_url', 'dashboard', 'side', 'high' );
+    add_meta_box( 'sc_system_confige_user_url', esc_html__( 'System Configuration (User URLs)', 'sc-system-configuration' ), 'sc_system_confige_user_url', 'dashboard', 'side', 'high' );
 }
 
 // hook to register the Admin dashboard widget
 add_action( 'wp_dashboard_setup', 'sc_add_dashboard_widgets' );
 
 /**
- * Output the html content of the dashboard widget
+ * Output the html content of the admin dashboard website url widget
  */
-function sc_system_configuration() {
-
+function sc_system_confige_website_url() {
   // detect the current user to get his phone number
   $user = wp_get_current_user();
   ?>
   <form id="sc_form" action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post" >
     <!-- controlls on which function the post will send -->
-    <input type="hidden" name="sc_action" id="sc_action" value="sc_user_data">
+    <input type="hidden" name="sc_action" id="sc_action" value="sc_website_url_data">
     <?php wp_nonce_field( 'sc_nonce', 'sc_nonce_field' ); ?>
     <div class="input-text-wrap" id="home-wrap">
-        <label for="home">Home Url:</label>
-        <input type="text" name="home" id="home" value="<?php echo esc_attr(get_option('home')); ?>" class="regular-text">
-      </div>
-
+      <label for="home">Home Url:</label>
+      <input type="text" name="home" id="home" value="<?php echo esc_attr(get_option('home')); ?>" class="regular-text">
+    </div>
     <div class="input-text-wrap" id="siteurl-wrap">
-        <label for="siteurl">Site Url:</label>
-        <input type="text" name="siteurl" id="siteurl" value="<?php echo esc_attr(get_option('siteurl')); ?>" class="regular-text">
+      <label for="siteurl">Site Url:</label>
+      <input type="text" name="siteurl" id="siteurl" value="<?php echo esc_attr(get_option('siteurl')); ?>" class="regular-text">
     </div>
     <div class="input-text-wrap" id="api-wrap">
-        <label for="api_url">Api Url:</label>
-        <input type="text" name="api_url" id="api_url" value="<?php echo esc_attr(get_option('api_url')); ?>" class="regular-text">
+      <label for="api_url">Api Url:</label>
+      <input type="text" name="api_url" id="api_url" value="<?php echo esc_attr(get_option('api_url')); ?>" class="regular-text">
     </div>
     <p>
       <input name="save-data" id="save-data" class="button button-primary" value="Save Settings" type="submit"> 
@@ -44,36 +40,90 @@ function sc_system_configuration() {
       <br class="clear">
     </p>
   </form>
+  <a href="<?php echo esc_url( admin_url( 'admin.php?page=system-configuration' ) ); ?>">System Configuration</a>
 <?php
 }
+
 /**
- * Saves the data from the admin widget
+ * Output the html content of the admin dashboard user url widget
  */
-function sc_save_user_data() {
-    $msg = '';
-    if(array_key_exists('nonce', $_POST) AND  wp_verify_nonce( $_POST['nonce'], 'sc_nonce' ) ) 
-    {
-       // detect the current user to get his phone number
-       $user = wp_get_current_user();
+function sc_system_confige_user_url() {
+  // detect the current user to get his phone number
+  $user = wp_get_current_user();
+  ?>
+  <form id="sc_user_url" action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post" >
+    <!-- controlls on which function the post will send -->
+    <input type="hidden" name="sc_user_url_action" id="sc_user_url_action" value="sc_user_url_data">
+    <?php wp_nonce_field( 'sc_nonce', 'sc_nonce_field' ); ?>
+    <div class="input-text-wrap" id="login-wrap">
+      <label for="login">Login:</label>
+      <input type="text" name="login" id="login" value="<?php echo esc_attr(get_option('login')); ?>" class="regular-text">
+    </div>
+    <div class="input-text-wrap" id="signup-wrap">
+      <label for="signup">Signup:</label>
+      <input type="text" name="signup" id="signup" value="<?php echo esc_attr(get_option('signup')); ?>" class="regular-text">
+    </div>
+    <p>
+      <input name="save-data" id="save-data" class="button button-primary" value="Save Settings" type="submit"> 
+      <a id="msg_u_u" style="margin-left: 10px;"></a>
+      <br class="clear">
+    </p>
+  </form>
+  <a href="<?php echo esc_url( admin_url( 'admin.php?page=system-configuration' ) ); ?>">System Configuration</a>
+<?php
+}
 
-       // change the phone number
-       update_option( 'home',     $_POST['home'] );
-       update_option( 'siteurl',  $_POST['siteurl'] );
-       update_option( 'api_url',  $_POST['api_url'] );
-
-       // success message
-       $msg = 'Settings Saved';
-    }
-    else
-    {   
-      // error message
-      $msg = 'Settings unsaved';
-    }
-    wp_send_json( $msg );
+/**
+ * Saves the data from the admin dashboard website url widget
+ */
+function sc_save_website_url_data() {
+  $msg = '';
+  if(array_key_exists('nonce', $_POST) AND wp_verify_nonce( $_POST['nonce'], 'sc_nonce' ) ) 
+  {
+    // Update theme opation
+    update_option( 'home',    $_POST['home'] );
+    update_option( 'siteurl', $_POST['siteurl'] );
+    update_option( 'api_url', $_POST['api_url'] );
+    // Success message
+    $msg = 'Settings Saved';
+  }
+  else
+  {
+    // Error message
+    $msg = 'Settings unsaved';
+  }
+  wp_send_json( $msg );
 }
 
 /**
  * ajax hook for registered users
  */
- add_action( 'wp_ajax_sc_user_data', 'sc_save_user_data' );
+ add_action( 'wp_ajax_sc_website_url_data', 'sc_save_website_url_data' );
+
+
+/**
+ * Saves the data from the admin dashboard user url widget
+ */
+function sc_save_user_url_data() {
+  $msg = '';
+  if(array_key_exists('nonce', $_POST) AND wp_verify_nonce( $_POST['nonce'], 'sc_nonce' ) ) 
+  {
+    // Update theme opation
+    update_option( 'login',    $_POST['login'] );
+    update_option( 'signup', $_POST['signup'] );
+    // Success message
+    $msg = 'Settings Saved';
+  }
+  else
+  {
+    // Error message
+    $msg = 'Settings unsaved';
+  }
+  wp_send_json( $msg );
+}
+
+/**
+ * ajax hook for registered users
+ */
+ add_action( 'wp_ajax_sc_user_url_data', 'sc_save_user_url_data' );
 
